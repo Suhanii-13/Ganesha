6ff1d8bc29f42e7f6dfe3a30d1730e4634365cde
suhani <suhanishaikh.1304@gmail.com>
2024-09-23 17:50:53 +0530

    added boilerplate

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require ("express");
 const app = express();
 const port = 8080;
+const ejsMate = require("ejs-mate");
 app.set("view engin" , "ejs");
 
 app.use(express.static("public"));
+app.engine('ejs', ejsMate);
+
 
 app.get("/" , (req,res)=>{
     res.render("index.ejs");
