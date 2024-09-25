4dfe96e1ed1a6efd23c72a86846a52c6d16e4ea8
suhani <suhanishaikh.1304@gmail.com>
2024-09-25 21:53:01 +0530

    update styling

--- public/js/colorPick.js
+++ public/js/colorPick.js
@@ -1,11 +1,9 @@
 document.getElementById("color1").addEventListener("input", updateStyles);
 document.getElementById("color2").addEventListener("input", updateStyles);
-document.getElementById("borderColor").addEventListener("input", updateStyles);
 
 function updateStyles() {
     const color1 = document.getElementById("color1").value;
     const color2 = document.getElementById("color2").value;
-    const borderColor = document.getElementById("borderColor").value;
 
     const gradients = document.querySelectorAll("linearGradient");
     gradients.forEach(gradient => {
@@ -13,8 +11,20 @@ function updateStyles() {
         gradient.children[1].setAttribute("stop-color", color2);
     });
 
-    const paths = document.querySelectorAll(".border");
-    paths.forEach(path => {
-        path.setAttribute("stroke", borderColor);
-    });
 }
+
+// const colorPicker = document.getElementById('bg');
+//     const body = document.body;
+
+//     colorPicker.addEventListener('input', function() {
+//         const selectedColor = colorPicker.value;
+
+//         body.style.backgroundImage = `
+//             url("../assets/bg.png"), 
+//             radial-gradient(
+//                 circle, 
+//                 rgba(173, 216, 230, 0.5) 30%,  /* Light blue center remains */
+//                 ${selectedColor} 80%           /* Update second color */
+//             )
+//         `;
+//     });
