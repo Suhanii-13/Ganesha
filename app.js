91944ef5d89525251c928fa1c38d9e618392bbdc
suhani <suhanishaikh.1304@gmail.com>
2024-09-29 22:33:49 +0530

    update code

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
+require('dotenv').config();
+
 const express = require("express");
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 const ejsMate = require("ejs-mate");
 
-app.set("view engine", "ejs"); 
-
+app.set("view engine", "ejs");
 app.engine('ejs', ejsMate);
-
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    res.render("index"); 
+    res.render("index");
 });
 
-app.listen(port, (req, res) => {
-    console.log(`App is listening on ${port}`);
+app.listen(port, () => {
+    console.log(`App is listening on port ${port}`);
 });
