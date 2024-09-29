8fdec28e9a64be5012bfb9d72e2b927044805252
suhani <suhanishaikh.1304@gmail.com>
2024-09-29 20:34:40 +0530

    final commit

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-const express = require ("express");
+const express = require("express");
 const app = express();
 const port = 8080;
 const ejsMate = require("ejs-mate");
-app.set("view engin" , "ejs");
 
-app.use(express.static("public"));
+app.set("view engine", "ejs"); 
+
 app.engine('ejs', ejsMate);
 
+app.use(express.static("public"));
 
-app.get("/" , (req,res)=>{
-    res.render("index.ejs");
+app.get("/", (req, res) => {
+    res.render("index"); 
 });
 
-app.listen(port,(req,res)=>{
-    console.log(`app is listning on ${port}` )
-})
-
+app.listen(port, (req, res) => {
+    console.log(`App is listening on ${port}`);
+});
